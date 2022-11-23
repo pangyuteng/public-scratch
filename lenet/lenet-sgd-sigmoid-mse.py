e837d7ba47ec25e7b6439dddac04d116bed6942f
pangyuteng <pteng@mednet.ucla.edu>
2022-11-23 10:42:36 -0800

    increased batch size to 128

--- lenet/lenet-sgd-sigmoid-mse.py
+++ lenet/lenet-sgd-sigmoid-mse.py
@@ -18,7 +18,7 @@ ds_train = ds_train.map(
     normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
 ds_train = ds_train.cache()
 ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-ds_train = ds_train.batch(1)
+ds_train = ds_train.batch(128)
 ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
 ds_test = ds_test.map(
