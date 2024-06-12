432bf9dddbbe18aa0c36559017922568e0f4ce09
pangyuteng <pangyuteng@gmail.com>
2024-06-12 22:01:06 +0000

    wip

--- docker/ajgong_deepmed/build_and_push.sh
+++ docker/ajgong_deepmed/build_and_push.sh
@@ -1,2 +1,4 @@
-docker build -t pangyuteng/ajgong_deep_med_custom_pd:0.1 .
-docker push pangyuteng/ajgong_deep_med_custom_pd:0.1
+docker build --build-arg="TF_TAG=1.15.0-gpu-py3" -t pangyuteng/ajgong_deep_med_custom_pd:tf1.15 .
+docker push pangyuteng/ajgong_deep_med_custom_pd:tf1.15
+#docker tag pangyuteng/ajgong_deep_med_custom_pd:tf1.15 pangyuteng/ajgong_deep_med_custom_pd:latest
+#docker push pangyuteng/ajgong_deep_med_custom_pd:latest
