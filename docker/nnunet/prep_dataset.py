cc305d438ed4159ddd86f2b5e6f888219ae719fb
pangyuteng <pangyuteng@gmail.com>
2024-09-04 16:24:13 -0700

    wip

---
+++ docker/nnunet/prep_dataset.py
@@ -0,0 +1,11 @@
+for x in range(40):
+    if x > 20:
+        target_img = 'imagesTr'
+        target_lb = 'labelsTr'
+    else:
+        target_img = 'imagesTs'
+        target_lb = 'labelsTs'
+
+        case_folder = f's{x:04d}'
+        command = f'cp {case_folder}/ct.nii.gz {target_img}/{x:04d}.nii.gz && cp {case_folder}/segmentations/brain.nii.gz {target_lb}/{x:04d}.nii.gz'
+        print(command)
--- docker/nnunet/run.sh
+++ docker/nnunet/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export nnUNet_raw="/radraid/pteng/tmp/test_nnUNet/data/nnUNet_raw"
-export nnUNet_preprocessed="/radraid/pteng/test_nnUNet/data/nnUNet_preprocessed"
-export nnUNet_results="/radraid/pteng/test_nnUNet/data/nnUNet_results"
+export nnUNet_raw="/radraid/pteng/tmp/nnUNet_raw"
+export nnUNet_preprocessed="/radraid/pteng/tmp/nnUNet_preprocessed"
+export nnUNet_results="/radraid/pteng/tmp/nnUNet_results"
 
 
 python -c "import torch;print(torch.__version__)"
+nnUNetv2_train 001 2d 0 -p hola
