82f264ed346e4195fff8268970acf4d51dc9d518
pangyuteng <pangyuteng@gmail.com>
2025-01-09 14:37:53 +0000

    wip

--- finance/basics/jiajun/jiajun.py
+++ finance/basics/jiajun/jiajun.py
@@ -4,27 +4,33 @@ import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="darkgrid")
 
 csv_file = '/mnt/hd2/data/finance/ohlc_1m_full_market/1y/2023-09-07-through-2024-09-06/SPY.csv'
 df = pd.read_csv(csv_file)
 print(df.columns)
 print(df.shape)
 
-
 df.start_time = df.start_time.apply(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S+00'))
 df.end_time = df.end_time.apply(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S+00'))
-df['start_date'] = df.start_time.apply(lambda x: x.date())
-df['the_time'] = df.start_time.apply(lambda x: x.time())
+df = df.sort_values(['start_time'])
+df['mydate'] = df.start_time.apply(lambda x: x.date())
+df['mytime'] = df.start_time.apply(lambda x: x.time())
+df['weekday'] = df.start_time.apply(lambda x: x.get_weekday())
+
+sns.lineplot(x="mytime",y="close",hue="weekday",data=df)
+plt.savefig('ok.png')
+
+"""
+#tstamp_list = pd.date_range(start="2000-01-01 09:30:00",end="2000-01-01 16:00:00",freq='m')
+date_list = sorted(list(df.mydate.unique()))
 mylist = []
+for date_item in date_list:
+    day_df = df[df.mydate==date_item].reset_index()
 
-for d in sorted(list(set([x.date() for x in df.start_time.unique()]))):
-    
-    day_df = df[df.start_date==d].reset_index()    
-    print(day_df.shape)
-
-    time_list = list([x.replace(year=2000,month=1,day=1) for x in day_df.start_date])
+    time_list = list([x.replace(year=2000,month=1,day=1) for x in day_df.mydate])
     price_list = np.array(list(day_df.close))
-
     if len(price_list) > 600:
         price_list = price_list/price_list[120]
         mylist.append(price_list[:600])
@@ -32,10 +38,10 @@ for d in sorted(list(set([x.date() for x in df.start_time.unique()]))):
 
 mylist = np.array(mylist)
 mean_price = np.median(mylist,axis=0)
+
+
 plt.plot(mean_price,linewidth=2)
 
 plt.grid(True)
 plt.savefig('ok.png')
-
-
-
+"""
