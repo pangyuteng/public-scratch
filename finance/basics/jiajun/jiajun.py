b6d92d1b36c38dc61672710b298e9201d1864fb2
pangyuteng <pangyuteng@gmail.com>
2025-01-09 18:40:01 +0000

    wip norm_price

--- finance/basics/jiajun/jiajun.py
+++ finance/basics/jiajun/jiajun.py
@@ -5,6 +5,7 @@ import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 import seaborn as sns
 sns.set_theme(style="darkgrid")
 
@@ -20,20 +21,42 @@ print(df.shape)
 # .replace(tzinfo=utc).astimezone(tz=eastern)
 # minute level csv file from unusualwhale, tstamp have "+00", but each day it starts at 8am, so likely eastern time.
 # london close 16:30 UCT, 10:30 ET, 8:30 PT.
-
+df = df[['start_time','close']]
 
 df.start_time = df.start_time.apply(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S+00'))
 df = df.sort_values(['start_time'])
-
-df['mydate'] = df.start_time.apply(lambda x: x.date())
-df['mytime'] = df.start_time.apply(lambda x: x.time())
-#df['seconds'] = df.start_time.apply(lambda x: (x.replace(year=2000,month=1,day=1)-datetime.datetime(2000,1,1,8,0,0,0)).total_seconds())
-df['seconds'] = df.start_time.apply(lambda x: x.replace(year=2000,month=1,day=1))
+df['norm_time'] = df.start_time.apply(lambda x: x.replace(year=2000,month=1,day=1))
+df['date_obj'] = df.start_time.apply(lambda x: x.date())
+df['time_obj'] = df.start_time.apply(lambda x: x.time())
 df['weekday'] = df.start_time.apply(lambda x: x.weekday())
-df['close'] = df['close'].astype(float)
+df = df[ (df.time_obj >= datetime.time(9,30)) & (df.time_obj < datetime.time(16,30)) ].reset_index()
 
-print(df['mytime'].unique())
-print(df['weekday'].unique())
 
-sns.lineplot(x="seconds",y="close",hue="weekday",data=df)
+london_close_price_dict ={}
+for date_item in list(df.date_obj.unique()):
+    tmp_df = df[(df.date_obj==date_item)&(df.time_obj>=datetime.time(11,28))&(df.time_obj<datetime.time(11,31))]
+    if len(tmp_df) > 0:
+        london_close_price_dict[date_item] = tmp_df.close.to_list()[-1]
+    else:
+        london_close_price_dict[date_item] = np.nan
+def get_london_close(x):
+    return london_close_price_dict[x]
+
+df['london_close']=df.date_obj.apply(lambda x: get_london_close(x))
+
+def get_norm_price(row):
+    return row.close/row.london_close
+
+df['norm_price'] = df.apply(lambda row: get_norm_price(row),axis=1)
+
+df = df[-5000:]
+print(df.head())
+print(df.shape)
+
+
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.lineplot(x="norm_time",y="norm_price",hue="weekday",data=df,ax=ax)
+xfmt = md.DateFormatter('%H:%M:%S')
+ax.xaxis.set_major_formatter(xfmt)
+ax.tick_params(axis='x', labelrotation=45)
 plt.savefig('intraday-spy.png')
